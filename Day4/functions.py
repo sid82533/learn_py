74111cc2abf49452e38d15116cc01613d2f95c94
sid82533 <sid82533@gmail.com>
2022-02-24 19:46:20 +0530

    Functions Basics

---
+++ Day4/functions.py
@@ -0,0 +1,24 @@
+# learning about python functions to clone the simple calculator by calling functions instead of repetative print statements
+# function without any arguments
+def my_func():
+    print("Hello Functions!")
+
+my_func()
+
+# functions with arguments
+
+def my_func2(a):
+    print(a)
+
+a = "Hello Arguments Function!"
+my_func2(a)
+
+# functions with 2 arguments
+
+def my_func3(a, b):
+    print(a + " " + b)
+
+a = "Hello"
+b = "World!"
+
+my_func3(a,b)
