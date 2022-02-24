1af30cb651df97a235ba42abfa7821e98d4c9007
sid82533 <sid82533@gmail.com>
2022-02-24 23:41:55 +0530

    More Strings

---
+++ Day3/slicing_strings.py
@@ -0,0 +1,14 @@
+# strings can be treated as  index, starting from 0. 
+
+a = "Hello World"
+print(a[2:5])
+
+# slice to the end
+
+b = "Not Hello World"
+print(b[2:])
+
+# negetive slice 
+
+c = "Welcome World!"
+print(c[-5:-2])
